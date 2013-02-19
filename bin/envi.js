fa6572d8cdc20e9c80a97dcfdd6895b211295cd6
Nicholas Faiz <nicholas.faiz@gmail.com>
2013-02-19 14:24:40 +1100

    create a conf dir, not config

--- bin/envi.js
+++ bin/envi.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
-fs.mkdirSync('config');
-fs.createReadStream('./../config/env.json').pipe(fs.createWriteStream('config/env.json'));
-fs.createReadStream('./../config/env.js').pipe(fs.createWriteStream('config/env.js'));
+fs.mkdirSync('conf');
+fs.createReadStream('./../config/env.json').pipe(fs.createWriteStream('conf/env.json'));
+fs.createReadStream('./../config/env.js').pipe(fs.createWriteStream('conf/env.js'));
 
 console.log("Done. Now run 'npm install nconf -save");
