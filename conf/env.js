2d0f0e20684f14a19de46e2f95fff7c5ac095854
Nicholas Faiz <nicholas.faiz@gmail.com>
2013-03-06 10:47:52 +1100

    Update env.js
    comma first

--- conf/env.js
+++ conf/env.js
@@ -1,5 +1,5 @@
-var nconf = require('nconf'),
-    path = require('path');
+var nconf = require('nconf')
+    , path = require('path');
 
 nconf.argv().env().file({
     file: path.join('config', 'env.json')
